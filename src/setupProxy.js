cb9aac22fd5624929373a07678099a766126bcfd
Sandra Jurberga-Šaudine <sandra.jurberga@gmail.com>
2022-08-03 21:50:10 +0300

    webpack

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,3 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {  app.use(    createProxyMiddleware(["/api", , "/otherApi"], { target: "https://nodeprojectreact.herokuapp.com/" })  );};
+module.exports = function(app) { 
+     app.use(    createProxyMiddleware(["/api", , "/otherApi"], { target: "https://nodeprojectreact.herokuapp.com/" }) 
+      );
+      app.use(entry= 'entry.js', output={ path: path.resolve(__dirname, 'dist'), filename: 'main.js' });
+    };
+
+ 
