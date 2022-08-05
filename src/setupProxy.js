c1bb0089ac2471bcb1b1e7dd079a1af63b92090f
Sandra Jurberga-Šaudine <sandra.jurberga@gmail.com>
2022-08-05 15:12:08 +0300

    m

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 module.exports = router;
 module.exports = function(app) { 
-     app.use(    createProxyMiddleware(["/api", , "/otherApi"], { target: "http://localhost:4000" }) 
+     app.use(    createProxyMiddleware(["/api", , "/otherApi"], { target: "https://nodeprojectreact.herokuapp.com" }) 
       );
       app.use(entry= 'entry.js', output={ path: path.resolve(__dirname, 'dist'), filename: 'main.js' });
     };
